a1cc2ced7675eef9cc0893d523f3ff591343e36c
MistbornL <lashaloria3@gmail.com>
2022-07-21 23:13:20 +0400

    secure dashboard

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,5 +1,26 @@
+import { getSession, signIn } from "next-auth/react";
+import { useEffect, useState } from "react";
+
 function Dashboard() {
-  return <h1>Dashboard page</h1>;
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const securePage = async () => {
+      const session = await getSession();
+      if (!session) {
+        signIn();
+      } else {
+        setLoading(false);
+      }
+    };
+    securePage();
+  }, []);
+
+  if (loading) {
+    <h2>Loading...</h2>;
+  } else {
+    return <h1>Welcome to Dashboard</h1>;
+  }
 }
 
 export default Dashboard;
