8964e97d2226debf09b3c5243cab3ecf7cedd5cb
MistbornL <lashaloria3@gmail.com>
2022-07-21 23:26:29 +0400

    server-side session

--- pages/blog.js
+++ pages/blog.js
@@ -1,5 +1,16 @@
-function Blog() {
-  return <h1>Blog page</h1>;
+import { getSession } from "next-auth/react";
+
+function Blog({ data }) {
+  return <h1>Blog page - {data}</h1>;
 }
 
 export default Blog;
+
+export async function getServerSideProps(context) {
+  const session = await getSession(context);
+  return {
+    props: {
+      data: session ? "list of 100 personalized posts" : "List of Free blogs",
+    },
+  };
+}
